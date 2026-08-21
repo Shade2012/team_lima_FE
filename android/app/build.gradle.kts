d62c450424ee0ab69ecfdc5aeae0f5c6a78eaa0e
Lintang <lintangwaladi@gmail.com>
2026-08-21 13:54:55 +0700

    fix(android): align package name with firebase console

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.veloce"
+    namespace = "com.example.team_five_fe"
     compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.veloce"
+        applicationId = "com.example.team_five_fe"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/team_five_fe/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/team_five_fe/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.veloce
+package com.example.team_five_fe
 
 import io.flutter.embedding.android.FlutterActivity
 
